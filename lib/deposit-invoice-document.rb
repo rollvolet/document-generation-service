d16465ab8277ec4f3c4de47f5cae89c4845db4c2
Erika Pauwels <erika.pauwels@gmail.com>
2018-12-12 16:55:40 +0100

    Add endpoint to generate a deposit invoice document

---
+++ lib/deposit-invoice-document.rb
@@ -0,0 +1,142 @@
+# coding: utf-8
+require 'wicked_pdf'
+require_relative './htmlentities'
+require_relative './helpers'
+
+module DocumentGenerator
+  class DepositInvoiceDocument
+
+    include DocumentGenerator::Helpers
+
+    def initialize
+      @inline_css = ''
+    end
+
+    def generate(path, data, language)
+      coder = HTMLEntities.new
+
+      template_path = select_template(data, language)
+      html = File.open(template_path, 'rb') { |file| file.read }
+
+      invoice_number = generate_invoice_number(data)
+      html.sub! '<!-- {{NUMBER}} -->', invoice_number
+
+      invoice_date = generate_invoice_date(data)
+      html.sub! '<!-- {{DATE}} -->', invoice_date
+
+      customer_number = data['customer']['number'].to_s
+      html.sub! '<!-- {{CUSTOMER_NUMBER}} -->', customer_number
+
+      own_reference = coder.encode(generate_own_reference(data), :named)
+      html.sub! '<!-- {{OWN_REFERENCE}} -->', own_reference
+
+      ext_reference = coder.encode(generate_ext_reference(data), :named)
+      html.sub! '<!-- {{EXT_REFERENCE}} -->', ext_reference
+
+      # TODO must be the embedded building instead of the referenced
+      building = coder.encode(generate_building(data), :named)
+      html.sub! '<!-- {{BUILDING}} -->', building
+
+      # TODO must be the embedded customer instead of the referenced
+      addresslines = coder.encode(generate_addresslines(data), :named)
+      html.sub! '<!-- {{ADDRESSLINES}} -->', addresslines
+
+      # TODO must be the embedded customer instead of the referenced
+      contactlines = coder.encode(generate_contactlines(data), :named)
+      html.sub! '<!-- {{CONTACTLINES}} -->', contactlines
+
+      request_number = generate_request_number(data)
+      html.sub! '<!-- {{REQUEST_NUMBER}} -->', request_number
+
+      pricing = generate_pricing(data)
+      html.sub! '<!-- {{INVOICELINES}} -->', pricing[:invoicelines]
+      html.sub! '<!-- {{TOTAL_NET_PRICE}} -->', format_decimal(pricing[:total_net_price])
+      html.sub! '<!-- {{VAT_RATE}} -->', format_vat_rate(pricing[:vat_rate])
+      html.sub! '<!-- {{TOTAL_VAT}} -->', format_decimal(pricing[:total_vat])
+      html.sub! '<!-- {{TOTAL_DEPOSIT_INVOICE}} -->', format_decimal(pricing[:total_deposit_invoice])
+
+      payment_due_date = generate_payment_due_date(data)
+      html.sub! '<!-- {{PAYMENT_DUE_DATE}} -->', payment_due_date
+
+      generate_certificate_notification(data)
+
+      html.sub! '<!-- {{INLINE_CSS}} -->', @inline_css
+
+      write_to_pdf(path, html, '/templates/offerte-footer.html')
+    end
+
+    def select_template(data, language)
+      if language == 'FRA'
+        ENV['DEPOSIT_INVOICE_TEMPLATE_FR'] || '/templates/voorschotfactuur-fr.html'
+      else
+        ENV['DEPOSIT_INVOICE_TEMPLATE_NL'] || '/templates/voorschotfactuur-nl.html'
+      end
+    end
+
+
+    def generate_own_reference(data)
+      order = data['order']
+      if order
+        offer = order['offer']
+        own_reference = "<b>AD #{order['requestNumber']}</b>"
+        own_reference += " <b>#{data['visit']['visitor']}</b>" if data['visit']
+        own_reference += "<br><span class='note'>#{offer['number']} #{offer['documentVersion']}</span>"
+        own_reference
+      else
+        hide_element('references--own_reference')
+      end
+    end
+
+    def generate_request_number(data)
+      order = data['order']
+      if order
+        "<b>AD #{order['requestNumber']}</b>"
+      else
+        hide_element('introduction--reference')
+      end
+    end
+
+    def generate_pricing(data)
+      invoicelines = []
+      prices = []
+
+      if data['order']
+        # TODO we assume all ordered offerlines have the same VAT rate as the invoice
+        data['order']['offer']['offerlines'].find_all { |l| l['isOrdered'] }.each do |offerline|
+          prices << offerline['amount']
+          line = "<div class='invoiceline'>"
+          line += "  <div class='col col-1'>#{offerline['description']}</div>"
+          line += "  <div class='col col-2'>&euro; #{format_decimal(offerline['amount'])}</div>"
+          line += "</div>"
+          invoicelines << line
+        end
+      end
+
+      total_net_price = prices.inject(:+) || 0
+      vat_rate = data['vatRate']['rate']
+      total_vat = total_net_price * vat_rate / 100
+      total_deposit_invoice = data['totalAmount']
+
+      {
+        invoicelines: invoicelines.join,
+        total_net_price: total_net_price,
+        vat_rate: vat_rate,
+        total_vat: total_vat,
+        total_deposit_invoice: total_deposit_invoice
+      }
+    end
+
+    def generate_payment_due_date(data)
+      if data['dueDate']
+        format_date(data['dueDate'])
+      else
+        hide_element('payment-notification--deadline')
+        ''
+      end
+    end
+
+    def generate_certificate_notification(data)
+      hide_element('certificate-notification') if not data['certificateRequired'] or data['certificateReceived']
+    end
+  end
+end
--- web.rb
+++ web.rb
@@ -7,6 +7,7 @@ require 'json'
 require_relative 'lib/visit-report'
 require_relative 'lib/offer-document'
 require_relative 'lib/invoice-document'
+require_relative 'lib/deposit-invoice-document'
 require_relative 'lib/vat-certificate'
 
 # TODO file cleanup job?
@@ -61,7 +62,7 @@ post '/documents/offer' do
   generator.generate(path, data)
 
   # TODO cleanup temporary created files of WickedPdf
-  
+
   send_file path
 end
 
@@ -69,7 +70,7 @@ post '/documents/invoice' do
   request.body.rewind
   json_body = JSON.parse request.body.read
 
-  # Workaround to embed visitor initals in the offer object
+  # Workaround to embed visitor initals in the invoice object
   data = json_body['invoice']
   data['visit'] = if json_body['visitor'] then { 'visitor' => json_body['visitor'] } else nil end
   language = json_body['language']
@@ -81,7 +82,27 @@ post '/documents/invoice' do
   generator.generate(path, data, language)
 
   # TODO cleanup temporary created files of WickedPdf
-  
+
+  send_file path
+end
+
+post '/documents/deposit-invoice' do
+  request.body.rewind
+  json_body = JSON.parse request.body.read
+
+  # Workaround to embed visitor initals in the deposit invoice object
+  data = json_body['invoice']
+  data['visit'] = if json_body['visitor'] then { 'visitor' => json_body['visitor'] } else nil end
+  language = json_body['language']
+
+  id = data['id']
+  path = "/tmp/#{id}-voorschotfactuur.pdf"
+
+  generator = DocumentGenerator::DepositInvoiceDocument.new
+  generator.generate(path, data, language)
+
+  # TODO cleanup temporary created files of WickedPdf
+
   send_file path
 end
 
@@ -99,6 +120,6 @@ post '/documents/certificate' do
   generator.generate(path, data, language)
 
   # TODO cleanup temporary created files of WickedPdf
-  
+
   send_file path
 end
